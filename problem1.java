2a915f152597f36574c0f1d77f77e9929603c8e8
Edward <tojonge@gmail.com>
2019-06-09 23:01:59 -0400

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -2,15 +2,20 @@ ProjectEuler
 ============
 
 problem1
-// Problem 1 Find the sum of all the multiples of 3 or 5 below 1000//   
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23
+Problem 1 Find the sum of all the multiples of 3 or 5 below 1000*/   
 public class ProjectEuler   {
-            public static void main(String[] args) {
-                int sum =0; 
-                for (int i=1; i <1000;i++) {
-                    if (i%3==0 || i%5==0){
-                        sum += i;
-                    }                   
-                }
-                        System.out.println("The answer is"+ " "+sum);
-    }    
-}
+ public static void main (String args[]) {
+System.out.println("All the natural numbers below 10 that are multiples of 3 or 5:");	
+int sum=0;
+		for(int j=0;j<1000;j++) {
+			if(j%3==0 || j%5==0) {	
+				sum=sum+j;
+System.out.println(j);
+			}
+		}		
+System.out.println("Sum of these multiples:"+sum);
+	}
+	 }
+           
